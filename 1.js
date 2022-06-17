69224ffb064de273f79da089d12ccf35cbc95267
Криворот Денис Сергеевич <1195117@mtp.by>
2022-06-17 15:18:48 +0300

    sec commit

---
+++ 1.js
@@ -0,0 +1,27 @@
+let arrOfInfphoto = [
+    "logo/BistroLog.png"
+]
+
+let arrOfInfText = [
+    "инф1",
+    "инф2",
+    "инф3",
+    "инф4",
+    "инф5",
+    "инф6",
+    "инф7",
+    "инф8",
+    "инф9"
+]
+
+var takensite = 0;
+function changeinf(taken) {
+    sessionStorage.myValue = taken;
+    document.location='2.html';
+
+
+}
+function ss(){
+    alert("вфыв");
+
+}
